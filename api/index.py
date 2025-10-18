69499bda64016c62254c1fb7f50f3a29a9b087cd
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-18 18:28:53 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,25 +1,11 @@
-import time
-import uuid
-
-from flask import Flask, session
-from flask_cors import CORS, cross_origin
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
-@app.route("/")
-@cross_origin()
-def index():
-    # if "user_id" not in session:
-    #     session["user_id"] = str(uuid.uuid4())
-    # return session["user_id"], 200
-    import db
-
-    return "Hello, World"
-
-
-@app.route("/api/shorten")
-@cross_origin()
-def shorten_url():
-    return {'time': time.time()}
+@app.route('/about')
+def about():
+    return 'About'
