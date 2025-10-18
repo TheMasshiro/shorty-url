22074958ab2e0ce17d14d339b368fb3de9c56644
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-18 18:21:45 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
+@cross_origin()
 def home():
     return 'Hello, World!'
 
 @app.route('/about')
+@cross_origin()
 def about():
     return 'About'
