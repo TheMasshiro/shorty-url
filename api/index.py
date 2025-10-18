7ee0269fd10982c265e480288ffac1f657556a84
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-18 18:09:26 +0800

    yeah

--- api/index.py
+++ api/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import time
+import uuid
+
+from flask import Flask, session
+from flask_cors import CORS, cross_origin
+from instance.config import Config
 
 app = Flask(__name__)
+CORS(app)
+app.config.from_object(Config)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/")
+@cross_origin()
+def index():
+    if "user_id" not in session:
+        session["user_id"] = str(uuid.uuid4())
+    return session["user_id"], 200
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route("/api/shorten")
+@cross_origin()
+def shorten_url():
+    return {'time': time.time()}
