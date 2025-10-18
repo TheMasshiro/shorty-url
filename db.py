381ded5114776146b3b154c1470d3eda0446e157
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-18 18:55:33 +0800

    test

--- db.py
+++ db.py
@@ -1,19 +1,18 @@
 import os
-import sqlite3
 
+import psycopg2
 from flask import g
+from psycopg2.extras import RealDictCursor
 
 from api.index import app
 
-DATABASE = os.path.join(os.getcwd(), "database.db")
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
     return db
-
 
 @app.teardown_appcontext
 def close_connection(_):
@@ -21,13 +20,14 @@ def close_connection(_):
     if db is not None:
         db.close()
 
-
 def check_database():
-    if not os.path.exists(DATABASE):
-        with app.app_context():
-            db = get_db()
-            with app.open_resource(os.path.join("schema.sql"), mode="r") as f:
-                db.cursor().executescript(f.read())
-            db.commit()
-            return True
-    return False
+    """Initialize database schema"""
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        with app.open_resource("schema.sql", mode="r") as f:
+            cursor.execute(f.read())
+        db.commit()
+        return True
+    except Exception:
+        return False
