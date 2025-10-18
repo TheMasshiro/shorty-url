6b3c791263a7cc1b509d3a12b7cb62efd93ff2eb
John Christian Vicente <johnc.vicente1@gmail.com>
2025-10-18 18:30:09 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
+import time
+
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
+@cross_origin()
 def home():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/api/shorten")
+@cross_origin()
+def shorten_url():
+    return {'time': time.time()}
